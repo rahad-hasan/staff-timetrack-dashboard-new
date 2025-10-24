9bd8585d9a8ee1c0b0054faca5268163cde61d50
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-10-24 16:17:39 +0600

    SEPARATED - sidebar items

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -1,15 +1,30 @@
 import {
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
 } from "@/components/ui/sheet"
+import Image from "next/image";
+import logo from '../../assets/logo.svg'
 
 const MobileSidebar = () => {
     return (
         <SheetContent>
             <SheetHeader>
-                <SheetTitle>Are you absolutely sure?</SheetTitle>
+                <SheetTitle>
+                    <div
+                        className={`flex items-center mb-4`}
+                    >
+                        <Image
+                            src={logo}
+                            alt="Logo"
+                            width={0}
+                            height={0}
+                            className={`w-10 h-10`}
+                        />
+                        <h2 className="text-xl font-bold">Tracker</h2>
+                    </div>
+                </SheetTitle>
                 <SheetDescription>
                     This action cannot be undone. This will permanently delete your account
                     and remove your data from our servers.
--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -6,128 +6,21 @@ import { useState } from 'react';
 import SidebarItem from './Sidebar/SidebarItem';
 import SubItem from './Sidebar/SubItem';
 import {
-    SquareActivity,
-    BarChart,
-    Lightbulb,
-    BriefcaseBusiness,
-    LayoutDashboard,
-    Clock4,
-    Users,
-    CalendarDays,
-    AlarmClock,
-    Settings,
     ChevronRight,
     ChevronLeft,
 } from 'lucide-react';
 import TrialCart from './Sidebar/TrialCart';
+import { othersSidebarItems, sidebarItems } from '@/utils/SidebarItems';
 
 const SideBar = () => {
     const [openMenu, setOpenMenu] = useState<string | null>('');
     const [activeSubItem, setActiveSubItem] = useState<string>('');
     const [isCollapsed, setIsCollapsed] = useState(false);
-    console.log(openMenu);
-    console.log(activeSubItem);
+    
     const toggleMenu = (menu: string) => {
         setOpenMenu((prev) => (prev === menu ? null : menu));
         setActiveSubItem('')
     };
-
-    const sidebarItems = [
-        {
-            icon: LayoutDashboard,
-            label: 'Dashboard',
-            key: '/dashboard',
-            collapsible: false,
-            subItems: [],
-        },
-        {
-            icon: Clock4,
-            label: 'Timesheets',
-            key: 'timesheets',
-            collapsible: true,
-            subItems: [
-                { label: 'All timesheets', key: '/timesheets/all-timesheets' },
-                { label: 'Manual requests', key: '/timesheets/manual-requests' },
-            ],
-        },
-        {
-            icon: SquareActivity,
-            label: 'Activity',
-            key: 'Activity',
-            collapsible: true,
-            subItems: [
-                { label: 'Screenshot', key: '/activity/screenshorts' },
-                { label: 'App', key: '/activity/app' },
-                { label: 'URLs', key: '/activity/urls' },
-            ],
-        },
-        {
-            icon: Lightbulb,
-            label: 'Insights',
-            key: '/insights',
-            collapsible: true,
-            subItems: [
-                { label: 'Highlight', key: '/insights/highlight' },
-            ],
-        },
-        {
-            icon: BriefcaseBusiness,
-            label: 'Project Management',
-            key: 'project-management',
-            collapsible: true,
-            subItems: [
-                { label: 'Projects', key: '/project-management/projects' },
-                { label: 'Task', key: '/project-management/task' },
-                { label: 'Clients', key: '/project-management/clients' },
-            ],
-        },
-        {
-            icon: BarChart,
-            label: 'Report',
-            key: 'report',
-            collapsible: true,
-            subItems: [
-                { label: 'Timesheet', key: '/report/timesheets' },
-                { label: 'Time & activities', key: '/report/time-and-activities' },
-                { label: 'Attendance', key: '/report/attendance' },
-            ],
-        },
-        {
-            icon: Users,
-            label: 'Teams',
-            key: '/teams',
-            collapsible: false,
-            subItems: [],
-        },
-
-    ];
-
-    const othersSidebarItems = [
-        {
-            icon: CalendarDays,
-            label: 'Calendar',
-            key: '/calender',
-            collapsible: false,
-            subItems: [],
-        },
-        {
-            icon: AlarmClock,
-            label: 'Leave Management',
-            key: '/leave-management',
-            collapsible: true,
-            subItems: [
-                { label: 'Leave details', key: '/leave-management/leave-details' },
-                { label: 'Leave request', key: '/leave-management/leave-request' },
-            ],
-        },
-        {
-            icon: Settings,
-            label: 'Settings',
-            key: '/settings',
-            collapsible: false,
-            subItems: [],
-        },
-    ]
 
     return (
         <div className={`${isCollapsed ? "w-[90px]" : "w-[320px]"} min-h-screen py-5  z-50 sticky top-0 flex flex-col transition-all duration-300`}>
