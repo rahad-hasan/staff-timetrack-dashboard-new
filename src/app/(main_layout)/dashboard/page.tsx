6bd801f8d12c516226235458d76db748a55c5ac9
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-11-08 00:23:47 +0600

    MADE - date dynamic based on selection in home page

--- src/app/(main_layout)/dashboard/page.tsx
+++ src/app/(main_layout)/dashboard/page.tsx
@@ -11,19 +11,69 @@ export default function Dashboard() {
     setActiveTab(tab);
   };
 
+  const getDailyDate = () => {
+    return new Date().toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    });
+  };
+
+  const getWeeklyDate = () => {
+    const startOfWeek = new Date();
+    const endOfWeek = new Date();
+
+    startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() + 1);
+    endOfWeek.setDate(startOfWeek.getDate() + 6);
+
+    const startDate = startOfWeek.toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    });
+
+    const endDate = endOfWeek.toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    });
+
+    return `${startDate} - ${endDate}`;
+  };
+
+  const getMonthlyDate = () => {
+    return new Date().toLocaleDateString("en-US", {
+      month: "short",
+      year: "numeric",
+    });
+  };
+
+  const date =
+    activeTab === "Daily"
+      ? getDailyDate()
+      : activeTab === "Weekly"
+      ? getWeeklyDate()
+      : activeTab === "Monthly"
+      ? getMonthlyDate()
+      : "";
+
   return (
     <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-5">
-      <HeadingComponent heading="Dashboard" subHeading="Mon, Aug 18, 2025 - Sun, Aug 24, 2025"></HeadingComponent>
+      <HeadingComponent heading="Dashboard" subHeading={date}></HeadingComponent>
 
       <div className="flex mt-3 sm:mt-0 bg-[#f6f7f9] dark:bg-darkSecondaryBg rounded-lg w-[240px] sm:w-auto">
         {["Daily", "Weekly", "Monthly"].map((tab) => (
           <button
             key={tab}
             onClick={() => handleTabClick(tab as "Daily" | "Weekly" | "Monthly")}
-            className={`px-4 py-2 text-sm font-medium transition-all cursor-pointer rounded-lg m-0.5 ${activeTab === tab
-              ? "bg-white dark:bg-darkPrimaryBg dark:text-darkTextPrimary text-headingTextColor shadow-sm"
-              : "text-gray-600 dark:text-darkTextPrimary hover:text-gray-800"
-              }`}
+            className={`px-4 py-2 text-sm font-medium transition-all cursor-pointer rounded-lg m-0.5 ${
+              activeTab === tab
+                ? "bg-white dark:bg-darkPrimaryBg dark:text-darkTextPrimary text-headingTextColor shadow-sm"
+                : "text-gray-600 dark:text-darkTextPrimary hover:text-gray-800"
+            }`}
           >
             {tab}
           </button>
