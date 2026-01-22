8ff5c709f90c8dee93205f7e8353e0594f7487b7
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2026-01-22 22:49:23 +0600

    FIXED - app urls api call without user_id

--- src/components/ProjectManagement/Task/TaskServer.tsx
+++ src/components/ProjectManagement/Task/TaskServer.tsx
@@ -2,13 +2,16 @@
 import { getTasks } from "@/actions/task/action";
 import TaskToggle from "./TaskToggle";
 import AppPagination from "@/components/Common/AppPagination";
+import { cookies } from "next/headers";
 
 const TaskServer = async ({ searchParams }: any) => {
+    const cookieStore = await cookies();
+    const userId = cookieStore.get("userId")?.value;
     const params = await searchParams;
     const result = await getTasks({
         search: params.search,
         project_id: params.project_id,
-        user_id: params.user_id,
+        user_id: params.user_id ?? userId,
         page: params.page,
     });
 
