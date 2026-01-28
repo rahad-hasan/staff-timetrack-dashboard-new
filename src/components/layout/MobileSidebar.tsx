e727332d82edae1d6354253aa3defa8ea4ecf4a7
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2026-01-29 00:55:18 +0600

    ADDED - new logo in mobile sidebar

--- src/components/layout/MobileSidebar.tsx
+++ src/components/layout/MobileSidebar.tsx
@@ -10,7 +10,8 @@ import { othersSidebarItems, sidebarItems, sidebarItemsEmployee } from "@/utils/
 import { useSidebarStore } from "@/store/sidebarStore";
 import MobileSidebarItem from "./sidebar/MobileSidebarItem";
 import MobileSubItem from "./sidebar/MobileSubItem";
-import timerLogo from '../../assets/timerLogo.svg';
+// import timerLogo from '../../assets/timerLogo.svg';
+import timerLogo from '../../assets/NewLogo.png'
 import { useLogInUserStore } from "@/store/logInUserStore";
 
 const MobileSidebar = () => {
@@ -41,12 +42,19 @@ const MobileSidebar = () => {
                             className={`w-10 h-10`}
                         />
                         <h2 className="text-xl font-bold text-headingTextColor dark:text-darkTextPrimary">Tracker</h2> */}
-                        <Image
+                        {/* <Image
                             src={timerLogo}
                             alt="Logo"
                             width={100}
                             height={100}
                             className={`h-10 w-10 bg-primary rounded-xl p-2 shadow-lg`}
+                        /> */}
+                        <Image
+                            src={timerLogo}
+                            alt="Logo"
+                            width={0}
+                            height={0}
+                            className={`h-auto my-2 px-1 w-12 py-1`}
                         />
                         <h2 className="text-2xl font-bold text-headingTextColor dark:text-darkTextPrimary">Tracker</h2>
                     </div>
