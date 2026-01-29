299b20edfb7d7adb236327bd7453845222c4ce4c
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2026-01-29 19:44:14 +0600

    FIXED - cart

--- src/components/Activity/ScreenShorts/Every10MinsServer.tsx
+++ src/components/Activity/ScreenShorts/Every10MinsServer.tsx
@@ -15,54 +15,54 @@ import ScreenshotActivityCard from "./ScreenshotActivityCard";
 // import AllScreenShortsSkeleton from "@/skeleton/activity/screenShorts/AllScreenShortsSkeleton";
 
 const Every10MinsServer = async ({ searchParams }: ISearchParamsProps) => {
-  const cookieStore = await cookies();
-  const userId = cookieStore.get("userId")?.value;
-  const params = await searchParams;
-  const currentDate = format(new Date(), "yyyy-MM-dd");
+    const cookieStore = await cookies();
+    const userId = cookieStore.get("userId")?.value;
+    const params = await searchParams;
+    const currentDate = format(new Date(), "yyyy-MM-dd");
 
-  const result = await getScreenshots10Min({
-    date: params.date ?? currentDate,
-    user_id: params.user_id ?? userId,
-    project_id: params?.project_id
-  });
+    const result = await getScreenshots10Min({
+        date: params.date ?? currentDate,
+        user_id: params.user_id ?? userId,
+        project_id: params?.project_id
+    });
 
-  return (
-    <div className="min-h-[80vh] xl:h-auto">
-      <div className="mb-5 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-4 gap-5">
-        <ScreenshotActivityCard
-          icon={WeeklyActivityColoredIcon}
-          value={result?.data?.score + "%"}
-          level="AVG ACTIVITY"
-          chart={FirstChart}
-          is_improved={true}
-          improved_value={"+1.5%"}
-        />
-        <ScreenshotActivityCard
-          icon={WeeklyWorkColoredIcon}
-          value={result?.data?.work_time}
-          level="WORKED TIME"
-          chart={SecondChart}
-          is_improved={true}
-          improved_value={"+30m"}
-        />
-        <ScreenshotActivityCard
-          icon={TotalProjectColoredIcon}
-          value={result?.data?.mouse_activity + "%"}
-          level="MOUSE ACTIVITY"
-          chart={FirstChart}
-          is_improved={false}
-          improved_value={"-2.5%"}
-        />
-        <ScreenshotActivityCard
-          icon={TeamMemberColoredIcon}
-          value={result?.data?.keyboard_activity + "%"}
-          level="KEYBOARD ACTIVITY"
-          chart={SecondChart}
-          is_improved={true}
-          improved_value={"+3.5%"}
-        />
+    return (
+        <div className="min-h-[80vh] xl:h-auto">
+            <div className="mb-5 grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-5">
+                <ScreenshotActivityCard
+                    icon={WeeklyActivityColoredIcon}
+                    value={result?.data?.score + "%"}
+                    level="AVG ACTIVITY"
+                    chart={FirstChart}
+                    is_improved={true}
+                    improved_value={"+1.5%"}
+                />
+                <ScreenshotActivityCard
+                    icon={WeeklyWorkColoredIcon}
+                    value={result?.data?.work_time}
+                    level="WORKED TIME"
+                    chart={SecondChart}
+                    is_improved={true}
+                    improved_value={"+30m"}
+                />
+                <ScreenshotActivityCard
+                    icon={TotalProjectColoredIcon}
+                    value={result?.data?.mouse_activity + "%"}
+                    level="MOUSE ACTIVITY"
+                    chart={FirstChart}
+                    is_improved={false}
+                    improved_value={"-2.5%"}
+                />
+                <ScreenshotActivityCard
+                    icon={TeamMemberColoredIcon}
+                    value={result?.data?.keyboard_activity + "%"}
+                    level="KEYBOARD ACTIVITY"
+                    chart={SecondChart}
+                    is_improved={true}
+                    improved_value={"+3.5%"}
+                />
 
-        {/* {metrics.map(({ id, value, title, change, direction, note }) => {
+                {/* {metrics.map(({ id, value, title, change, direction, note }) => {
                     const isUp = direction === "up";
                     const TrendIcon = isUp ? TrendingUp : TrendingDown;
                     const trendColor = isUp ? "text-[#12cd69]" : "text-[#f40139]";
@@ -119,13 +119,13 @@ const Every10MinsServer = async ({ searchParams }: ISearchParamsProps) => {
                         </div>
                     );
                 })} */}
-      </div>
-      <Suspense fallback={<Every10MinsSkeleton />}>
-        {<Every10Mins data={result?.data?.interval_rows} />}
-      </Suspense>
+            </div>
+            <Suspense fallback={<Every10MinsSkeleton />}>
+                {<Every10Mins data={result?.data?.interval_rows} />}
+            </Suspense>
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Every10MinsServer;
--- src/components/Activity/ScreenShorts/ScreenshotActivityCard.tsx
+++ src/components/Activity/ScreenShorts/ScreenshotActivityCard.tsx
@@ -18,24 +18,24 @@ function ScreenshotActivityCard({
   improved_value,
 }: IScreenshotActivityCardProps) {
   return (
-    <div className="rounded-2xl w-full  transition-all hover:shadow duration-200 relative h-38 shadow-sm dark:shadow-slate-100">
-      <div className="border-x-2 border-t-2 border-borderColor/40 dark:border-darkBorder/25 flex items-center justify-between px-4 py-6 bg-bgPrimary dark:bg-darkPrimaryBg rounded-t-2xl">
-        <div className=" flex items-center gap-3">
+    <div className="rounded-2xl w-full  transition-all hover:shadow duration-200 relative h-34 2xl:h-40 shadow-sm dark:shadow-slate-100">
+      <div className="border-x-2 border-t-2 border-borderColor/40 dark:border-darkBorder/25 flex items-center justify-between px-3 2xl:px-4 py-[1.4rem] 2xl:py-[1.8rem] bg-bgPrimary dark:bg-darkPrimaryBg rounded-t-2xl">
+        <div className=" flex items-center gap-2 2xl:gap-3">
           <Icon size={36} />
           <div>
-            <h2 className="text-2xl font-medium text-headingTextColor dark:text-darkTextPrimary">
+            <h2 className="text-lg sm:text-2xl xl:text-xl 2xl:text-2xl font-medium text-headingTextColor dark:text-darkTextPrimary">
               {value}
             </h2>
-            <h3 className=" uppercase text-subTextColor dark:text-darkTextSecondary">
+            <h3 className=" uppercase text-md xl:text-sm 2xl:text-[15px] text-subTextColor dark:text-darkTextSecondary">
               {level}
             </h3>
           </div>
         </div>
-        <div className={is_improved ? "text-[#12cd69]" : "text-red-600"}>
+        <div className={` w-16 sm:w-18 h-9 xl:w-[55px] xl:h-9 2xl:w-22 2xl:h-12 ${is_improved ? "text-[#12cd69]" : "text-red-600"}`}>
           <Chart color={is_improved ? "#3BC1A8" : "#FF0000"}></Chart>
         </div>
       </div>
-      <div className="bg-bgSecondary dark:bg-darkSecondaryBg rounded-b-2xl border-t px-4 py-3 flex items-center gap-2 absolute left-0 right-0 bottom-0">
+      <div className="bg-bgSecondary dark:bg-darkSecondaryBg rounded-b-2xl border-t px-3 2xl:px-4 py-2 sm:py-2.5 2xl:py-3 flex items-center gap-2 absolute left-0 right-0 bottom-0">
         {is_improved ? (
           <TrendingUp size={20} className={"text-[#12cd69]"} />
         ) : (
