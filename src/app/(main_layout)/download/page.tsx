5151c8dbae6c0f933dfb43a1a40b6093f42b6179
naimuddin94 <developernaimuddin@gmail.com>
2026-01-30 15:02:01 +0600

    client modal button rename

--- src/app/(main_layout)/download/page.tsx
+++ src/app/(main_layout)/download/page.tsx
@@ -1,80 +1,89 @@
-import windows from '../../../assets/download/download-windows-remove.png'
-import linux from '../../../assets/download/download-linux-remove-bg.png'
-import apple from '../../../assets/download/download-apple-remove.png'
-import Image from 'next/image';
-import { Button } from '@/components/ui/button';
-import { Download as DownloadIcon } from 'lucide-react';
+import windows from "../../../assets/download/download-windows-remove.png";
+import linux from "../../../assets/download/download-linux-remove-bg.png";
+import apple from "../../../assets/download/download-apple-remove.png";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Download as DownloadIcon } from "lucide-react";
 
 const DownloadPage = () => {
-    const LINUX_DOWNLOAD_URL = "/apps/linux/staff-time-tracker_0.0.1.deb";
-    const MACOS_DOWNLOAD_URL = "/apps/macos/staff-time-tracker_0.0.1.exe";
-    const WINDOWS_DOWNLOAD_URL = "/apps/windows/staff-time-tracker_0.0.1.exe";
+  const LINUX_DOWNLOAD_URL = "/apps/linux/staff-time-tracker_0.0.1.deb";
+  const MACOS_DOWNLOAD_URL = "/apps/macos/staff-time-tracker_0.0.1.exe";
+  const WINDOWS_DOWNLOAD_URL = "/apps/windows/staff-time-tracker_0.0.1.dmg";
 
-    return (
-        <div className='container mx-auto py-4 sm:py-8 lg:py-12'>
-            <div className='mb-8'>
-                <h1 className="text-center text-xl sm:text-2xl lg:text-3xl font-medium text-gray-900 dark:text-darkTextPrimary">
-                    Download Staff Time Tracker
-                </h1>
-                <p className="text-center mt-2 text-sm sm:text-md text-gray-600 dark:text-darkTextSecondary">
-                    Staff Time Tracker is available for Windows, macOS, and Linux
-                </p>
-                <p className="text-center mt-1 text-sm text-gray-500 dark:text-darkTextSecondary">
-                    Current Version: 0.0.1
-                </p>
-            </div>
+  return (
+    <div className="container mx-auto py-4 sm:py-8 lg:py-12">
+      <div className="mb-8">
+        <h1 className="text-center text-xl sm:text-2xl lg:text-3xl font-medium text-gray-900 dark:text-darkTextPrimary">
+          Download Staff Time Tracker
+        </h1>
+        <p className="text-center mt-2 text-sm sm:text-md text-gray-600 dark:text-darkTextSecondary">
+          Staff Time Tracker is available for Windows, macOS, and Linux
+        </p>
+        <p className="text-center mt-1 text-sm text-gray-500 dark:text-darkTextSecondary">
+          Current Version: 0.0.1
+        </p>
+      </div>
 
-            <div className='flex flex-col sm:flex-row justify-center sm:items-end gap-12 lg:gap-20 xl:gap-32'>
-
-                <div className='flex flex-col items-center gap-3'>
-                    <Image
-                        src={windows}
-                        width={120}
-                        height={120}
-                        alt='Windows OS icon'
-                    />
-                    <div className='text-lg font-medium'>Windows</div>
-                    <a href={LINUX_DOWNLOAD_URL} download="staff-time-tracker_0.0.1.exe" target="_self" rel="noopener">
-                        <Button size={'sm'} variant={'download'} className='text-sm'>
-                            <DownloadIcon className='w-4 h-4 mr-1' /> Download
-                        </Button>
-                    </a>
-                </div>
-
-                <div className='flex flex-col items-center gap-3'>
-                    <Image
-                        src={linux}
-                        width={140}
-                        height={140}
-                        alt='Linux OS icon'
-                        className=' -mb-4'
-                    />
-                    <div className='text-lg font-medium'>Linux</div>
-                    <a href={WINDOWS_DOWNLOAD_URL} download="staff-time-tracker_0.0.1.deb" target="_self" rel="noopener">
-                        <Button size={'sm'} variant={'download'} className='text-sm'>
-                            <DownloadIcon className='w-4 h-4 mr-1' /> Download
-                        </Button>
-                    </a>
-                </div>
-
-                <div className='flex flex-col items-center gap-3'>
-                    <Image
-                        src={apple}
-                        width={120}
-                        height={120}
-                        alt='macOS OS icon'
-                        className="dark:invert"
-                    />
-                    <div className='text-lg font-medium'>macOS</div>
-                    <a href={MACOS_DOWNLOAD_URL} download="staff-time-tracker_0.0.1.exe" target="_self" rel="noopener">
-                        <Button size={'sm'} variant={'download'} className='text-sm'>
-                            <DownloadIcon className='w-4 h-4 mr-1' /> Download
-                        </Button>
-                    </a>
-                </div>
-            </div>
+      <div className="flex flex-col sm:flex-row justify-center sm:items-end gap-12 lg:gap-20 xl:gap-32">
+        <div className="flex flex-col items-center gap-3">
+          <Image src={windows} width={120} height={120} alt="Windows OS icon" />
+          <div className="text-lg font-medium">Windows</div>
+          <a
+            href={LINUX_DOWNLOAD_URL}
+            download="staff-time-tracker_0.0.1.exe"
+            target="_self"
+            rel="noopener"
+          >
+            <Button size={"sm"} variant={"download"} className="text-sm">
+              <DownloadIcon className="w-4 h-4 mr-1" /> Download
+            </Button>
+          </a>
         </div>
-    );
+
+        <div className="flex flex-col items-center gap-3">
+          <Image
+            src={linux}
+            width={140}
+            height={140}
+            alt="Linux OS icon"
+            className=" -mb-4"
+          />
+          <div className="text-lg font-medium">Linux</div>
+          <a
+            href={WINDOWS_DOWNLOAD_URL}
+            download="staff-time-tracker_0.0.1.deb"
+            target="_self"
+            rel="noopener"
+          >
+            <Button size={"sm"} variant={"download"} className="text-sm">
+              <DownloadIcon className="w-4 h-4 mr-1" /> Download
+            </Button>
+          </a>
+        </div>
+
+        <div className="flex flex-col items-center gap-3">
+          <Image
+            src={apple}
+            width={120}
+            height={120}
+            alt="macOS OS icon"
+            className="dark:invert"
+          />
+          <div className="text-lg font-medium">macOS</div>
+          <a
+            href={MACOS_DOWNLOAD_URL}
+            download="staff-time-tracker_0.0.1.exe"
+            target="_self"
+            rel="noopener"
+          >
+            <Button size={"sm"} variant={"download"} className="text-sm">
+              <DownloadIcon className="w-4 h-4 mr-1" /> Download
+            </Button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DownloadPage;
--- src/components/ProjectManagement/Clients/AddClientModal.tsx
+++ src/components/ProjectManagement/Clients/AddClientModal.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import {
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-} from "@/components/ui/dialog"
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { newClientSchema } from "@/zod/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Resolver, useForm } from "react-hook-form";
 import z from "zod";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "sonner";
@@ -24,131 +24,156 @@ import { useState } from "react";
 import { addClient } from "@/actions/clients/action";
 
 const AddClientModal = ({ onClose }: { onClose: () => void }) => {
-    const [loading, setLoading] = useState(false);
-    const form = useForm<z.infer<typeof newClientSchema>>({
-        resolver: zodResolver(newClientSchema) as Resolver<z.infer<typeof newClientSchema>>,
-        defaultValues: {
-            name: "",
-            address: "",
-            email: "",
-            phone: ""
+  const [loading, setLoading] = useState(false);
+  const form = useForm<z.infer<typeof newClientSchema>>({
+    resolver: zodResolver(newClientSchema) as Resolver<
+      z.infer<typeof newClientSchema>
+    >,
+    defaultValues: {
+      name: "",
+      address: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof newClientSchema>) {
+    setLoading(true);
+    try {
+      const res = await addClient(values);
+
+      if (res?.success) {
+        setTimeout(() => {
+          onClose();
+        }, 100);
+        form.reset();
+        toast.success(res?.message || "Client added successfully");
+      } else {
+        toast.error(res?.message || "Failed to add client", {
+          style: {
+            backgroundColor: "#ef4444",
+            color: "white",
+            border: "none",
+          },
+        });
+      }
+    } catch (error: any) {
+      console.error("failed:", error);
+      toast.error(error.message || "Something went wrong!", {
+        style: {
+          backgroundColor: "#ef4444",
+          color: "white",
+          border: "none",
         },
-    })
-
-    async function onSubmit(values: z.infer<typeof newClientSchema>) {
-        setLoading(true);
-        try {
-            const res = await addClient(values);
-
-            if (res?.success) {
-                setTimeout(() => {
-                    onClose();
-                }, 100);
-                form.reset();
-                toast.success(res?.message || "Client added successfully");
-            } else {
-                toast.error(res?.message || "Failed to add client", {
-                    style: {
-                        backgroundColor: '#ef4444',
-                        color: 'white',
-                        border: 'none'
-                    },
-                });
-            }
-        } catch (error: any) {
-            console.error("failed:", error);
-            toast.error(error.message || "Something went wrong!", {
-                style: {
-                    backgroundColor: '#ef4444',
-                    color: 'white',
-                    border: 'none'
-                },
-            });
-        } finally {
-            setLoading(false);
-        }
+      });
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return (
-        <DialogContent
-            onInteractOutside={(event) => event.preventDefault()}
-            className="sm:max-w-[525px]">
-            <DialogHeader>
-                <DialogTitle className=" mb-4 text-headingTextColor dark:text-darkTextPrimary">Add New Client</DialogTitle>
-            </DialogHeader>
+  return (
+    <DialogContent
+      onInteractOutside={(event) => event.preventDefault()}
+      className="sm:max-w-[525px]"
+    >
+      <DialogHeader>
+        <DialogTitle className=" mb-4 text-headingTextColor dark:text-darkTextPrimary">
+          Add New Client
+        </DialogTitle>
+      </DialogHeader>
 
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 ">
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Name</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="dark:bg-darkPrimaryBg dark:border-darkBorder" placeholder="Client Name" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="address"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Address</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="dark:bg-darkPrimaryBg dark:border-darkBorder" placeholder="Enter Address" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="email"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Email</FormLabel>
-                                <FormControl>
-                                    <Input type="text" className="dark:bg-darkPrimaryBg dark:border-darkBorder" placeholder="Enter Email" {...field} />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="phone"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Phone</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        type="text"
-                                        placeholder="Enter Phone Number"
-                                        className="dark:bg-darkPrimaryBg dark:border-darkBorder"
-                                        {...field}
-                                        onChange={(e) => {
-                                            // This Regex allows ONLY the '+' sign and numbers 0-9
-                                            // It replaces any other character (like 'a', 'b', '!', etc.) with an empty string
-                                            const sanitizedValue = e.target.value.replace(/[^\d+]/g, "");
-                                            field.onChange(sanitizedValue);
-                                        }}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    {/* <DialogClose asChild> */}
-                    <Button className="w-full" disabled={loading} type="submit">{loading ? "Loading..." : "Create Task"}</Button>
-                    {/* </DialogClose> */}
-                </form>
-            </Form>
-        </DialogContent>
-    );
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 ">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="dark:bg-darkPrimaryBg dark:border-darkBorder"
+                    placeholder="Client Name"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Address</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="dark:bg-darkPrimaryBg dark:border-darkBorder"
+                    placeholder="Enter Address"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    className="dark:bg-darkPrimaryBg dark:border-darkBorder"
+                    placeholder="Enter Email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone</FormLabel>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Enter Phone Number"
+                    className="dark:bg-darkPrimaryBg dark:border-darkBorder"
+                    {...field}
+                    onChange={(e) => {
+                      // This Regex allows ONLY the '+' sign and numbers 0-9
+                      // It replaces any other character (like 'a', 'b', '!', etc.) with an empty string
+                      const sanitizedValue = e.target.value.replace(
+                        /[^\d+]/g,
+                        "",
+                      );
+                      field.onChange(sanitizedValue);
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* <DialogClose asChild> */}
+          <Button className="w-full" disabled={loading} type="submit">
+            {loading ? "Loading..." : "Add Client"}
+          </Button>
+          {/* </DialogClose> */}
+        </form>
+      </Form>
+    </DialogContent>
+  );
 };
 
 export default AddClientModal;
