b381b1577adea1e7bea5fe1283f9e415927a940e
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2026-04-26 10:49:09 +0600

    UPDATED - DayWeekMonth picker

--- src/components/Common/DayWeekMonthSelection.tsx
+++ src/components/Common/DayWeekMonthSelection.tsx
@@ -1,17 +1,24 @@
 "use client"
+import { cn } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useTopLoader } from "nextjs-toploader";
 
+type Tab = "daily" | "weekly" | "monthly";
 const DayWeekMonthSelection = () => {
     const loader = useTopLoader();
-    type Tab = "daily" | "weekly" | "monthly";
     const searchParams = useSearchParams();
     const router = useRouter();
     const activeTab = (searchParams.get("tab") as Tab) ?? "daily";
+
     const setTab = (tab: Tab) => {
         const params = new URLSearchParams();
+        const userId = searchParams.get("user_id");
+        if (userId) {
+            params.set("user_id", userId);
+        }
         params.set("tab", tab);
-        router.push(`?${params.toString()}`);
+        loader.start()
+        router.push(`?${params}`);
     };
     return (
         <div className="grid grid-cols-3 lg:flex mt-3 w-[250px] lg:w-auto sm:mt-0 bg-bgSecondary dark:bg-darkSecondaryBg rounded-lg box-border ">
@@ -20,13 +27,13 @@ const DayWeekMonthSelection = () => {
                     key={tab}
                     onClick={() => {
                         setTab(tab as "daily" | "weekly" | "monthly")
-                        loader.start()
                     }}
-                    className={`px-3.5 h-10 text-sm font-medium transition-all cursor-pointer rounded-lg min-w-[70px] text-center capitalize
-                        ${activeTab === tab
-                            ? "bg-bgPrimary dark:bg-darkPrimaryBg dark:text-darkTextPrimary text-headingTextColor outline-1 outline-borderColor dark:outline-darkBorder"
-                            : " text-headingTextColor dark:text-darkTextPrimary hover:text-gray-800"
-                        }`}
+                    className={cn(
+                        "px-3.5 h-10 text-sm font-medium transition-all cursor-pointer rounded-lg min-w-[70px] text-center capitalize",
+                        activeTab === tab
+                            ? "bg-bgPrimary dark:bg-darkPrimaryBg text-headingTextColor dark:text-darkTextPrimary outline-1 outline-borderColor dark:outline-darkBorder"
+                            : "text-headingTextColor dark:text-darkTextPrimary hover:text-gray-800"
+                    )}
                 >
                     {tab}
                 </button>
