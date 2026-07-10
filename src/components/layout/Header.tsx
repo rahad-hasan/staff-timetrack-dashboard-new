524b725f58e14099a91257661c10b5f788b0d650
naimuddin94 <developernaimuddin@gmail.com>
2026-07-10 15:17:54 +0600

    notification count issue solve

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,12 +8,9 @@ import { Button } from "../ui/button";
 //     Popover,
 //     PopoverTrigger,
 // } from "@/components/ui/popover"
-import { CirclePlay, Download, Menu, } from "lucide-react";
+import { CirclePlay, Download, Menu } from "lucide-react";
 // import StartTimer from "./Header/StartTimer";
-import {
-    Sheet,
-    SheetTrigger,
-} from "@/components/ui/sheet"
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 import MobileSidebar from "./MobileSidebar";
 import DarkMoodToggle from "./Header/DarkMoodToogle";
 import ProfileDropDown from "./Header/ProfileDropDown";
@@ -24,71 +21,88 @@ import { socket } from "@/socket/socket";
 // import bellIcon from '../../assets/header/bell.svg'
 
 const Header = ({ data }: { data: { duration: string } }) => {
-    // const [open, setOpen] = useState(false);
-    const [notificationsList, setNotificationsList] = useState<any[]>([]);
-    const unreadCount = notificationsList.filter(n => n.is_read === false).length;
+  // const [open, setOpen] = useState(false);
+  const [notificationsList, setNotificationsList] = useState<any[]>([]);
+  const [unreadCount, setUnreadCount] = useState(0);
 
+  useEffect(() => {
+    const handleIncomingData = (data: any) => {
+      const newList = Array.isArray(data) ? data : data?.data || [];
 
-    useEffect(() => {
-        const handleIncomingData = (data: any) => {
-            const newList = Array.isArray(data) ? data : data?.data || [];
+      setUnreadCount(data ? data?.meta?.total : 0);
+      setNotificationsList(newList);
+    };
+    socket.on("notifications", handleIncomingData);
+    return () => {
+      socket.off("notifications", handleIncomingData);
+    };
+  }, []);
 
-            setNotificationsList(newList);
-        };
-        socket.on("notifications", handleIncomingData);
-        return () => {
-            socket.off("notifications", handleIncomingData);
-        };
-    }, []);
+  // console.log(notificationsList);
 
-    // console.log(notificationsList);
-
-    return (
-        <div className=" border-b border-borderColor dark:border-darkBorder py-3 lg:py-3.5 2xl:py-5 px-3 lg:px-5 flex items-center justify-between rounded-t-lg dark:bg-darkPrimaryBg">
-            <div>
-                {/* <Popover open={open} onOpenChange={setOpen} modal>
+  return (
+    <div className=" border-b border-borderColor dark:border-darkBorder py-3 lg:py-3.5 2xl:py-5 px-3 lg:px-5 flex items-center justify-between rounded-t-lg dark:bg-darkPrimaryBg">
+      <div>
+        {/* <Popover open={open} onOpenChange={setOpen} modal>
                     <PopoverTrigger asChild>
                         <Button className=" dark:border-darkBorder dark:hover:bg-darkSecondaryBg" variant={'filter'}><CirclePlay className="text-primary size-5.5 sm:size-5.5" /><span className=" hidden lg:block dark:text-darkTextPrimary">Start Timer</span></Button>
                     </PopoverTrigger>
                     <StartTimer onClose={() => setOpen(false)}></StartTimer>
                 </Popover> */}
-                <Button className=" dark:border-darkBorder dark:hover:bg-darkSecondaryBg cursor-default" variant={'filter'}><CirclePlay className="text-primary size-5.5 sm:size-5.5" /><span className="  dark:text-darkTextPrimary">{data?.duration}</span></Button>
-            </div>
+        <Button
+          className=" dark:border-darkBorder dark:hover:bg-darkSecondaryBg cursor-default"
+          variant={"filter"}
+        >
+          <CirclePlay className="text-primary size-5.5 sm:size-5.5" />
+          <span className="  dark:text-darkTextPrimary">{data?.duration}</span>
+        </Button>
+      </div>
 
-            <div className=" hidden lg:flex items-center gap-4">
-                <div>
-                    <DarkMoodToggle></DarkMoodToggle>
-                </div>
-                <div className="border-x-2 border-borderColor dark:border-darkBorder px-3">
-                    <Notification unreadCount={unreadCount} notificationsList={notificationsList}></Notification>
-                </div>
-                <div className="hidden lg:block ">
-                    {/* <Button className=" dark:border-primary " variant={'outline'}><Image src={downloadIcon} width={0} height={0} className="w-5" alt="download" />Download App</Button> */}
-                    <Link href={`/download`}>
-                        <Button className=" dark:border-primary dark:hover:bg-darkSecondaryBg" variant={'outline'}><Download className=" text-primary w-8" />Download App</Button>
-                    </Link>
-                </div>
-                <ProfileDropDown></ProfileDropDown>
-            </div>
+      <div className=" hidden lg:flex items-center gap-4">
+        <div>
+          <DarkMoodToggle></DarkMoodToggle>
+        </div>
+        <div className="border-x-2 border-borderColor dark:border-darkBorder px-3">
+          <Notification
+            unreadCount={unreadCount}
+            notificationsList={notificationsList}
+          ></Notification>
+        </div>
+        <div className="hidden lg:block ">
+          {/* <Button className=" dark:border-primary " variant={'outline'}><Image src={downloadIcon} width={0} height={0} className="w-5" alt="download" />Download App</Button> */}
+          <Link href={`/download`}>
+            <Button
+              className=" dark:border-primary dark:hover:bg-darkSecondaryBg"
+              variant={"outline"}
+            >
+              <Download className=" text-primary w-8" />
+              Download App
+            </Button>
+          </Link>
+        </div>
+        <ProfileDropDown></ProfileDropDown>
+      </div>
 
-            {/* mobile menu */}
-            <div className=" flex items-center gap-4 lg:hidden">
-                <div>
-                    <DarkMoodToggle></DarkMoodToggle>
-                </div>
+      {/* mobile menu */}
+      <div className=" flex items-center gap-4 lg:hidden">
+        <div>
+          <DarkMoodToggle></DarkMoodToggle>
+        </div>
 
-                <div className="">
-                    <Notification></Notification>
-                </div>
+        <div className="">
+          <Notification></Notification>
+        </div>
 
-                <ProfileDropDown></ProfileDropDown>
-                <Sheet>
-                    <SheetTrigger asChild><Menu className=" cursor-pointer " size={20} /></SheetTrigger>
-                    <MobileSidebar></MobileSidebar>
-                </Sheet>
-            </div>
-        </div >
-    );
+        <ProfileDropDown></ProfileDropDown>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Menu className=" cursor-pointer " size={20} />
+          </SheetTrigger>
+          <MobileSidebar></MobileSidebar>
+        </Sheet>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
