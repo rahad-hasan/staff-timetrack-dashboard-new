f69751f6f51fce3bec5963ee4af0a4119a34ac85
naimuddin94 <developernaimuddin@gmail.com>
2026-08-25 09:27:55 +0600

    socker url added

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_API_URL, {
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
   autoConnect: false,
 });
